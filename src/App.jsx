67fb850b174a941459d510d609227bb0d900a529
maks <121092431+RYLE-307@users.noreply.github.com>
2026-01-20 08:07:46 +0300

    second

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import About from './pages/About';
 import Products from './pages/Products';
